e42760fac624cf4e94eeb44746d46e69b676c746
mana-yamada <yamanasshi6771@gmail.com>
2020-03-25 00:51:42 +0900

    HelloUsersystemメッセージ変更１
    Signed-off-by: mana-yamada <yamanasshi6771@gmail.com>

---
+++ myapp/app2/src/user_management_2/HelloUserSystem.java
@@ -0,0 +1,11 @@
+package user_management_2;
+
+public class HelloUserSystem {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world......");
+		System.out.println("Hello world222....");
+	}
+
+}

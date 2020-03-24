8d63de8a0739ba12d92a946aa24a0a28e6172334
mana-yamada <yamanasshi6771@gmail.com>
2020-03-25 01:43:27 +0900

    3行追加
    Signed-off-by: mana-yamada <yamanasshi6771@gmail.com>

--- myapp/app2/src/user_management_2/HelloUserSystem.java
+++ myapp/app2/src/user_management_2/HelloUserSystem.java
@@ -5,6 +5,9 @@ public class HelloUserSystem {
 	public static void main(String[] args) {
 
 		System.out.println("Hello world88888888......");
+		System.out.println("Hello world88888888......");
+		System.out.println("Hello world88888888......");
+		System.out.println("Hello world88888888......");
 	}
 
 }
